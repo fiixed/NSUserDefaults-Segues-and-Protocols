c9cfbde2c3c8df4007c87a8f87e9b82298b6ac36
Andrew Bell <fiixed@gmail.com>
2014-10-05 11:00:27 -0500

    Initial Commit

---
+++ NSUserDefaults, Segues and Protocols Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSUserDefaults, Segues and Protocols Challenge Solution
+//
+//  Created by Andrew Bell on 10/5/14.
+//  Copyright (c) 2014 Andrew Bell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSUserDefaults, Segues and Protocols Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSUserDefaults, Segues and Protocols Challenge Solution
+//
+//  Created by Andrew Bell on 10/5/14.
+//  Copyright (c) 2014 Andrew Bell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
